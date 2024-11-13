92e3862248679946e3692de0fe6ca31a326ec9b6
BondarevTK <BondarevTK101@gmail.com>
2024-11-13 17:20:04 +0300

    Task5

---
+++ Tyuiu.BondarevTK.Sprint2.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.BondarevTK.Sprint2.Task5.V7.Lib;
+namespace Tyuiu.BondarevTK.Sprint2.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string res = ds.FindMonthName(1990, 2);
+            Assert.AreEqual(res, "Февраль");
+        }
+    }
+}
---
+++ Tyuiu.BondarevTK.Sprint2.Task5.V7/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.BondarevTK.Sprint2.Task5.V7.Lib;
+namespace Tyuiu.BondarevTK.Sprint2.Task5.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ds.FindMonthName(x, y));
+        }
+    }
+}
