55f895e54ac68a4c975322c1f5784bc92c9f6c44
BondarevTK <BondarevTK101@gmail.com>
2024-11-13 16:23:43 +0300

    Task3

---
+++ Tyuiu.BondarevTK.Sprint2.Task3.V27.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.BondarevTK.Sprint2.Task3.V27.Lib;
+namespace Tyuiu.BondarevTK.Sprint2.Task3.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(-10);
+            Assert.AreEqual(1.0201, res);
+        }
+    }
+}
---
+++ Tyuiu.BondarevTK.Sprint2.Task3.V27/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BondarevTK.Sprint2.Task3.V27.Lib;
+
+namespace Tyuiu.BondarevTK.Sprint2.Task3.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            DataService ds = new DataService();
+            Console.WriteLine(ds.Calculate(x));
+
+        }
+    }
+}
